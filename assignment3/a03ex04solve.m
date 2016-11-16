77fdd5fe1d1655991cbb72ce0f302397a38e5a7b
ManuelWiddel <manuel_widdel@yahoo.de>
2016-11-16 15:05:01 +0100

    Update a03ex04solve.m

--- assignment3/a03ex04solve.m
+++ assignment3/a03ex04solve.m
@@ -1,38 +1,18 @@
+% Assignment 03, Exercise 04c) by Georgia Markouleki(387232), Manuel Widdel(379704), Mohammad Teimoori(370543)
 function error = a03ex04solve()
 
 a = 1; %these parameters are independant of p
 b = 4;
 c = 1;
 
-for p = 1:13 %for p > 13, error message: "Out of memory"
+for p = 1:15
     
     N = 2.^p - 1;
     h = 1 / (N + 1);
     
-    Sparse_a = zeros(N,N);
-    Sparse_b = zeros(N,N);
+    Sparse_a = sparse(gallery('tridiag',N,1,-2,1));
+    Sparse_b = sparse(gallery('tridiag',N,-1,0,1));
     Sparse_c = speye(N,N);
-
-    for i = 1:N
-        for j = 1:N
-            if i == j
-                Sparse_a(i,j) = -2;
-            end
-            if j+1 == i
-                    Sparse_a(i,j) = 1;
-                    Sparse_b(i,j) = -1;
-            end
-            if i+1 == j
-                        Sparse_a(i,j) = 1;
-                        Sparse_b(i,j) = 1;
-            end
-
-        end
-    end
-
-    Sparse_a = sparse(Sparse_a);
-
-    Sparse_b = sparse(Sparse_b);
 
     Lh = -a / h.^2 * Sparse_a - b / (2 * h) * Sparse_b + c * Sparse_c;
 
@@ -41,9 +21,8 @@ for p = 1:13 %for p > 13, error message: "Out of memory"
     
     if p == 1 %this if condition makes sure that the arrays don't get overwritten on every increase of p
     error_abs = zeros(N,1); 
-    error = zeros(13,1);
-    error_log = zeros(13,1);
-    h_array = zeros(13,1);
+    error = zeros(15,1);
+    h_array = zeros(15,1);
     end
     
 
