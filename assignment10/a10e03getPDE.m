50f6a0ab224424808c1892fec1712fd8fc6f48ff
Teimoori <teimoori.m@gmail.com>
2017-02-01 22:24:29 +0100

    10 & 11 initial

--- assignment10/a10e03getPDE.m
+++ assignment10/a10e03getPDE.m
@@ -1,46 +1,32 @@
+% Assignment 10, by Georgia Markouleki(387232), Manuel Widdel(379704), Mohammad Teimoori(370543) 
 function [x,U] = a10e03getPDE(a,b,alpha,beta,f,N)
 syms x;
-phi=(beta-alpha)/(b-a)*(x-a)+alpha;
-phider1=diff(phi);
-phider2=diff(diff(phi));
-for i=1:N, 
-    psi(1,i)=x^(i-1); 
+n = N+2; % number of points
+nel = n-1; % number of elements
+h = (b-a)/(nel); % Element length
+x_vec = a:h:b;
+
+% inital shape functions
+for i=1:n-1
+    shape_funcs(i,1) = hat(x_vec(i),i,h);
 end
-psi=(x-a)*(x-b)*psi;
-psider1=diff(psi);
-psider2=diff(diff(psi));
-psitrans=transpose(psi);
-E1=-psider2+psi;
-E2=-phider2+phi-f;
-c=-(int(psitrans*E1,x,a,b))\int(psitrans*E2,x,a,b);
-e=E1*c+E2;
-
-approx=phi+psi*c;
-x=(a-10):.2:(b+10);
-y_approx=subs(approx);
-approx=phi+psi*c;
-x=(a-10):.2:(b+10);
-y_approx=subs(approx);
-
-clear x;
-syms x;
-exac=dsolve('a2*D2y+a1*Dy+a0*y=f','y(a)=phi1','y(b)=phi2','x');
-exac=subs(exac);
-x=(a-10):.2:(b+10);
-y_exac=subs(exac);
-
-e_norm2=sqrt(int(e^2,a,b));
-f_norm2=sqrt(int(approx^2,a,b));
-err=abs(vpa(e_norm2/f_norm2*100,2));
-
-plot(x,y_approx,'-o','Color','r'),grid
-axis([a-pi b+pi -100 100])
-hold on
-title('\fontsize{14} Solutions of the Ordinary Differential Equation through Galerkin Method')
-xlabel('x')
-ylabel('\Phi(x)')
-plot(x,y_exac),grid
-legend('Approximative solution','Exact solution')
-hold off
-grid on
+%construct LHS
+A = zeros(n,n);
+for i=1:2:n-1
+    A(i,i) = int(diff(shape_funcs(1), x)*diff(shape_funcs(1), x) - shape_funcs(1)*shape_funcs(1), x_vec(i),x_vec(i+1));
+    A(i,i+1) = int(diff(shape_funcs(1), x)*diff(shape_funcs(2), x) - shape_funcs(1)*shape_funcs(2), x_vec(i),x_vec(i+1));
+    A(i+1,i) = int(diff(shape_funcs(2), x)*diff(shape_funcs(1), x) - shape_funcs(2)*shape_funcs(1), x_vec(i),x_vec(i+1));
+    A(i+1,i+1) = int(diff(shape_funcs(2), x)*diff(shape_funcs(2), x) - shape_funcs(2)*shape_funcs(2), x_vec(i),x_vec(i+1));
+end
+%construct RHS: missing
+%soling system: missing
+end
+% shape function on elements
+function [phi] = hat(xi,i,h)
+    syms x    
+    if(mod(i,2)==1)
+        phi = (x - xi)/h;
+    else
+        phi = (xi - x)/h;
+    end
 end
---
+++ assignment11/a11e02pyramid.m
@@ -0,0 +1,9 @@
+function [phi] = a11e02pyramid(i,j,h,order)
+    num_elements = 1/h;
+    xi = i*h;
+    yi = j*h;
+    a = zeros(1,num_elements);
+    for el_iter=1:num_elements
+       
+    end
+end
